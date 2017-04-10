c510c76aa37a6c181ff89da6db3465afd3e78608
jacobwilliams1 <jacob.williams@interapthq.com>
2017-04-10 16:24:13 -0400

    added maply

--- guessingGame.js
+++ guessingGame.js
@@ -67,27 +67,6 @@ $("#reset").on("click", function(){
             location.reload();
 });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function isNumberKey(evt) {
    var charCode = (evt.which) ? evt.which : event.keyCode;
    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
